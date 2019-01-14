12ba4f98431638033def313d9ca100c45cb77efd
Makai Zoltán <makaizoltan1990@gmail.com>
2019-01-14 19:52:45 +0100

    Revert "BT54 A Better Component Architecture"
    This reverts commit 12d369f36066a919864a39b6af6594512670790e.

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,12 +6,10 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
-class UTankTrack;
-
 /**
- * Responsible for driving the tank tracks
+ * 
  */
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+UCLASS()
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -19,14 +17,6 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
-
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void Initialise(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet);
-	
-private:
-	UTankTrack * LeftTrack = nullptr;
-	UTankTrack * RightTrack = nullptr;
 	
 	
-
 };
