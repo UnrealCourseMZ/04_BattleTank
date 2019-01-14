a57f27c7c56e1bd7ea587dd0ae535ab99a0948e1
Makai Zoltán <makaizoltan1990@gmail.com>
2019-01-14 21:57:57 +0100

    BT53 Using BlueprintReadOnly

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -11,6 +11,7 @@ class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
 class AProjectile;
+class UTankMovementComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -19,6 +20,9 @@ class BATTLETANK_API ATank : public APawn
 
 protected:
 	UTankAimingComponent * TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	UTankMovementComponent * TankMovementComponent = nullptr;
 
 private:	
 	// Sets default values for this pawn's properties
