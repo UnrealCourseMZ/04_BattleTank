a43c59abf416dfa1d640cb0ea9c3b54053d67cb7
Makai Zoltán <makaizoltan1990@gmail.com>
2019-12-04 21:02:44 +0100

    BT90 Using TakeDamage() on Actors

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -7,6 +7,8 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 #include "PhysicsEngine/RadialForceComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/DamageType.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -33,6 +35,9 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = Setup)
 	float DestroyDelay = 10.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float ProjectileDamage = 20.f;
 
 	UProjectileMovementComponent * ProjectileMovement = nullptr;
 
