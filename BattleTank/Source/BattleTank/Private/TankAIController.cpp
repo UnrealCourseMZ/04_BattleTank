6ee437fc7406d5662d44a12993639221cc8c268b
Makai Zoltán <makaizoltan1990@gmail.com>
2019-12-15 20:20:28 +0100

    BT92 The Observer Pattern

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,11 +2,31 @@
 
 #include "TankAIController.h"
 #include "TankAimingComponent.h"
+#include "Tank.h" // so we can implement OnDeath
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 }
+
+void ATankAIController::SetPawn(APawn * InPawn)
+{
+	Super::SetPawn(InPawn);
+	if (InPawn)
+	{
+		auto PossessedTank = Cast<ATank>(InPawn);
+		if (!ensure(PossessedTank)) { return; }
+
+		// TODO Subscribe our local method to the tank's death event
+		PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnPossessedTankDeath);
+	}
+}
+
+void ATankAIController::OnPossessedTankDeath()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Received!"))
+}
+
 
 void ATankAIController::Tick(float DeltaTime)
 {
@@ -29,6 +49,3 @@ void ATankAIController::Tick(float DeltaTime)
 		AimingComponent->Fire(); // TODO limit firing rate
 	}
 }
-
-
-
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -22,8 +22,11 @@ protected:
 private:
 	virtual void BeginPlay() override;
 
+	virtual void SetPawn(APawn* InPawn) override;
+
 	virtual void Tick(float DeltaTime) override;
 
-
-
+	UFUNCTION()
+	void OnPossessedTankDeath();
+	
 };
